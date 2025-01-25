3ce29830938e9e5febdae4c29ca7e9643ee1b549
Jean-Marc R-Mahoro <jean_marcc@hotmail.com>
2025-01-24 22:20:28 -0400

    scaffold-eth2 initialized

--- packages/foundry/contracts/YourContract.sol
+++ packages/foundry/contracts/YourContract.sol
@@ -1,79 +1,125 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "forge-std/console.sol";
+// // Useful for debugging. Remove when deploying to a live network.
+// import "forge-std/console.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+// // Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
+// // import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-    // State Variables
-    address public immutable owner;
-    string public greeting = "Building Unstoppable Apps!!!";
-    bool public premium = false;
-    uint256 public totalCounter = 0;
-    mapping(address => uint256) public userGreetingCounter;
+// /**
+//  * A smart contract that allows changing a state variable of the contract and tracking the changes
+//  * It also allows the owner to withdraw the Ether in the contract
+//  * @author BuidlGuidl
+//  */
+// contract YourContract {
+//     // State Variables
+//     address public immutable owner;
+//     string public greeting = "Building Unstoppable Apps!!!";
+//     bool public premium = false;
+//     uint256 public totalCounter = 0;
+//     mapping(address => uint256) public userGreetingCounter;
 
-    // Events: a way to emit log statements from smart contract that can be listened to by external parties
-    event GreetingChange(address indexed greetingSetter, string newGreeting, bool premium, uint256 value);
+//     // Events: a way to emit log statements from smart contract that can be listened to by external parties
+//     event GreetingChange(address indexed greetingSetter, string newGreeting, bool premium, uint256 value);
 
-    // Constructor: Called once on contract deployment
-    // Check packages/foundry/deploy/Deploy.s.sol
-    constructor(address _owner) {
-        owner = _owner;
+//     // Constructor: Called once on contract deployment
+//     // Check packages/foundry/deploy/Deploy.s.sol
+//     constructor(address _owner) {
+//         owner = _owner;
+//     }
+
+//     // Modifier: used to define a set of rules that must be met before or after a function is executed
+//     // Check the withdraw() function
+//     modifier isOwner() {
+//         // msg.sender: predefined variable that represents address of the account that called the current function
+//         require(msg.sender == owner, "Not the Owner");
+//         _;
+//     }
+
+//     /**
+//      * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
+//      *
+//      * @param _newGreeting (string memory) - new greeting to save on the contract
+//      */
+//     function setGreeting(string memory _newGreeting) public payable {
+//         // Print data to the anvil chain console. Remove when deploying to a live network.
+
+//         console.logString("Setting new greeting");
+//         console.logString(_newGreeting);
+
+//         greeting = _newGreeting;
+//         totalCounter += 1;
+//         userGreetingCounter[msg.sender] += 1;
+
+//         // msg.value: built-in global variable that represents the amount of ether sent with the transaction
+//         if (msg.value > 0) {
+//             premium = true;
+//         } else {
+//             premium = false;
+//         }
+
+//         // emit: keyword used to trigger an event
+//         emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
+//     }
+
+//     /**
+//      * Function that allows the owner to withdraw all the Ether in the contract
+//      * The function can only be called by the owner of the contract as defined by the isOwner modifier
+//      */
+//     function withdraw() public isOwner {
+//         (bool success,) = owner.call{ value: address(this).balance }("");
+//         require(success, "Failed to send Ether");
+//     }
+
+//     /**
+//      * Function that allows the contract to receive ETH
+//      */
+//     receive() external payable { }
+// }
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact efixatlantic@gmail.com
+contract YourContract is
+    ERC20,
+    ERC20Burnable,
+    ERC20Pausable,
+    Ownable,
+    ERC20Permit
+{
+    constructor(
+        address initialOwner
+    )
+        ERC20("WorldCapital", "WCL")
+        Ownable(initialOwner)
+        ERC20Permit("WorldCapital")
+    {}
+
+    function pause() public onlyOwner {
+        _pause();
     }
 
-    // Modifier: used to define a set of rules that must be met before or after a function is executed
-    // Check the withdraw() function
-    modifier isOwner() {
-        // msg.sender: predefined variable that represents address of the account that called the current function
-        require(msg.sender == owner, "Not the Owner");
-        _;
+    function unpause() public onlyOwner {
+        _unpause();
     }
 
-    /**
-     * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-     *
-     * @param _newGreeting (string memory) - new greeting to save on the contract
-     */
-    function setGreeting(string memory _newGreeting) public payable {
-        // Print data to the anvil chain console. Remove when deploying to a live network.
-
-        console.logString("Setting new greeting");
-        console.logString(_newGreeting);
-
-        greeting = _newGreeting;
-        totalCounter += 1;
-        userGreetingCounter[msg.sender] += 1;
-
-        // msg.value: built-in global variable that represents the amount of ether sent with the transaction
-        if (msg.value > 0) {
-            premium = true;
-        } else {
-            premium = false;
-        }
-
-        // emit: keyword used to trigger an event
-        emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
-    /**
-     * Function that allows the owner to withdraw all the Ether in the contract
-     * The function can only be called by the owner of the contract as defined by the isOwner modifier
-     */
-    function withdraw() public isOwner {
-        (bool success,) = owner.call{ value: address(this).balance }("");
-        require(success, "Failed to send Ether");
-    }
+    // The following functions are overrides required by Solidity.
 
-    /**
-     * Function that allows the contract to receive ETH
-     */
-    receive() external payable { }
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
 }
